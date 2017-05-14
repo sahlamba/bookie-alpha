5d4a67647b1c3d89dd92e7e8b7b8d5886da454b0
Sahil Lamba <sahil.lamba95@gmail.com>
2017-05-15 01:58:30 +0530

    Get loggedUser from fire, reconnect to fire on each open

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -4,24 +4,49 @@ import React, { Component } from 'react';
 import {
   Alert,
   View,
+  Image,
   Text,
 } from 'react-native';
 
 import FBSDK, { AccessToken } from 'react-native-fbsdk';
+import firebase from 'firebase';
 import { Actions, ActionConst } from 'react-native-router-flux';
 import styles from '../../styles/Test';
+
+const auth = firebase.auth();
+const provider = firebase.auth.FacebookAuthProvider;
 
 export default class Test extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      currentUser: {}
+    };
   }
 
   componentWillMount() {
     AccessToken.getCurrentAccessToken()
       .then((token) => {
-        if (!token)
+        if (!token) {
+          return false;
+        } else {
+          const credential = provider.credential(token.accessToken);
+          return auth.signInWithCredential(credential);
+        }
+      })
+      .then((isConnected) => {
+        if (isConnected) {
+          let currentUser = {};
+          currentUser.name = auth.currentUser.displayName;
+          currentUser.email = auth.currentUser.email;
+          currentUser.photoUrl = auth.currentUser.photoURL;
+          currentUser.uid = auth.currentUser.uid;
+          this.setState({currentUser});
+        } else {
+          console.log('Redirecting to login...');
           Actions.login();
+        }
       })
       .catch((err) => {
         console.error(err);
@@ -31,8 +56,14 @@ export default class Test extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>
-          Welcome to home!
+        <Image
+          style={{width: 100, height: 100, borderRadius: 50, marginBottom: 20}}
+          source={{uri: this.state.currentUser.photoUrl}} />
+        <Text style={{fontWeight: 'bold'}}>
+          {this.state.currentUser.name}
+        </Text>
+        <Text>
+          {this.state.currentUser.email}
         </Text>
         <Text
           style={styles.text}
