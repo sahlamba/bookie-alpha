ab5093d44448fed9bb2ca852b6849d448a6b6399
Sahil Lamba <sahil.lamba95@gmail.com>
2017-05-17 01:01:19 +0530

    Try: Add rightButton in NavBar

--- index.android.js
+++ index.android.js
@@ -12,12 +12,15 @@ import {
   StatusBar,
   NetInfo,
   View,
+  Text,
 } from 'react-native';
 
 // 3rd Party
 import { Router, Scene, Actions, ActionConst, Modal } from 'react-native-router-flux';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 // App
+import design from './src/styles/Design';
 import firebaseApp from './src/services/Firebase';
 import StatusModal from './src/components/StatusModal';
 import Splash from './src/scenes/splash/Splash';
@@ -51,13 +54,21 @@ export default class bookie extends Component {
   };
 
   render() {
+    const rightButton = () => (
+      <View style={styles.rightButton}>
+        <Icon name="ios-search" size={20} color="#333" style={styles.icon} />
+        <Text style={styles.text}>Search</Text>
+      </View>
+    );
     return (
-      <Router>
+      <Router
+        navigationBarStyle={styles.navBar}
+        titleStyle={styles.titleStyle}>
         <Scene key="modal" component={Modal} >
           <Scene key="root">
             <Scene key="splash" component={Splash} title="Welcome" initial={true} hideNavBar />
             <Scene key="login" component={Login} title="Login" type={ActionConst.RESET} hideNavBar />
-            <Scene key="home" component={Home} title="Explore" hideNavBar={false} />
+            <Scene key="home" component={Home} title="Explore" hideNavBar={false} renderRightButton={rightButton} />
           </Scene>
           <Scene key="statusModal" component={StatusModal} />
         </Scene>
@@ -72,7 +83,29 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFFFFF',
-  }
+  },
+  navBar: {
+    height: 53,
+    backgroundColor: '#FAFAFA',
+    borderBottomWidth: 0,
+  },
+  titleStyle: {
+    color: design.colors.primary,
+  },
+  rightButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingRight: 10,
+  },
+  text: {
+    color: '#333',
+  },
+  icon: {
+    marginTop: 0,
+    marginRight: 10,
+  },
 });
 
 AppRegistry.registerComponent('bookie', () => bookie);
